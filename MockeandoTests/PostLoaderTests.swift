f6ccc7481fa691d5dbdcc2ab8333ffe47dcc7fad
Franklin Samboni Castillo <sambonifranklin@gmail.com>
2022-12-15 22:41:59 -0500

    Add Post entity

--- MockeandoTests/PostLoaderTests.swift
+++ MockeandoTests/PostLoaderTests.swift
@@ -8,6 +8,15 @@
 import XCTest
 import Mockeando
 
+struct Post {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
 final class PostLoaderTests: XCTestCase {
+    
+    
     
 }
