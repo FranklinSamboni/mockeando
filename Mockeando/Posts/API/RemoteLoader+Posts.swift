3289fd3566f912b4f90a34d7272f101d1d4a71f1
Franklin Samboni Castillo <sambonifranklin@gmail.com>
2022-12-18 10:56:18 -0500

    Move mappers to their owns files

--- Mockeando/Posts/API/RemoteLoader+Posts.swift
+++ Mockeando/Posts/API/RemoteLoader+Posts.swift
@@ -18,36 +18,3 @@ extension RemoteLoader: PostLoader {
         load(tryMap: PostAPIMapper.mapPost(_:), completion: completion)
     }
 }
-
-private enum PostAPIMapper {
-    static func mapPosts(_ data: Data) throws -> [Post] {
-        let decoded = try JSONDecoder().decode([Payload].self, from: data)
-        let posts = Self.map(items: decoded)
-        return posts
-    }
-    
-    static func mapPost(_ data: Data) throws -> Post {
-        let decoded = try JSONDecoder().decode(Payload.self, from: data)
-        let posts = Self.map(item: decoded)
-        return posts
-    }
-    
-    private static func map(items: [Payload]) -> [Post] {
-        items.map { Self.map(item: $0) }
-    }
-    
-    private static func map(item: Payload) -> Post {
-        Post(userId: item.userId,
-             id: item.id,
-             title: item.title,
-             body: item.body,
-             isFavorite: false)
-    }
-    
-    private struct Payload: Codable {
-        let userId: Int
-        let id: Int
-        let title: String
-        let body: String
-    }
-}
